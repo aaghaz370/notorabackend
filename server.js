046fbcfcc469962dae69096622048701a321f763
aaghaz afzal <aghazafzal8294@gmail.com>
2025-11-08 03:34:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bookRoutes from "./routes/bookRoutes.js";
 import commentRoutes from "./routes/comments.js";
+import morgan from "morgan";
+
 
 
 
@@ -11,10 +13,11 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(morgan("dev"));
 app.use("/api/books", bookRoutes);
 app.use("/api/comments", commentRoutes);
 
 
 
 app.listen(9090, () => console.log("🚀 Notora user backend running"));
+
