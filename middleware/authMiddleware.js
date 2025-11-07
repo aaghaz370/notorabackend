09e58b0c4e5b42d28f668c76b94faf0a6673b443
aaghaz afzal <aghazafzal8294@gmail.com>
2025-11-08 03:25:26 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+
 
 export const protect = async (req, res, next) => {
   let token;
@@ -23,3 +23,4 @@ export const admin = (req, res, next) => {
   if (req.user && req.user.isAdmin) next();
   else res.status(403).json({ message: "Admin access required" });
 };
+
